bdd162ef128679ee43cd0f2a1222116a275213e4
pvgonzales <pvgonzales@up.edu.ph>
2024-05-05 14:33:54 +0800

    chore: updated log in functionalities

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
 // pages/Login.jsx
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    fetchUsers();
+  }, [])
+
+  const fetchUsers = () => {
+    axios.get('http://localhost:3001/register').then((res) => {
+      console.log(res.data) // Just for checking, must remove this later
+    })
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Implement login functionality here
+    try {
+      const response = await axios.post('http://localhost:3001/login', { email, password })
+      const token = response.data.token
+      setEmail('')
+      setPassword('')
+      fetchUsers()
+      navigate('/customer-page')
+      window.location.reload()
+      localStorage.setItem('token', token)
+    } catch(error) {
+      console.log('Unable to log in')
+    }
   };
 
   return (
@@ -34,8 +58,7 @@ function Login() {
             required
           />
         </div>
-        {/* CAN STILL CHANGE */}
-        <Link to="/customer-page"><button type="submit">Login</button></Link>
+        <button type="submit">Login</button>
       </form>
       <p>
         Don't have an account? <Link to="/register">Register</Link>
