7ede7208ed8c7ee5803c7567cf845736347417c1
naanareta <naanareta@up.edu.ph>
2024-05-06 21:24:56 +0800

    ui: explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,21 @@
 export default function Explore(){
     return(
         <div className="app-container">
-            <aside>
+            <div className="about">
                 <h1>ABOUT</h1>
-            </aside>
-            <p>Content</p>
+                <p>"Farm-to-table" is a dynamic social movement that champions a direct and transparent connection between consumers 
+                    and the agricultural producers who supply their food. It represents a departure from the conventional industrial 
+                    food system, where products often pass through numerous intermediaries before reaching the consumer's plate. 
+                    At its core, the farm-to-table movement aims to promote sustainability, support local economies, and prioritize 
+                    the quality and freshness of ingredients. By shortening the supply chain, consumers can have greater confidence 
+                    in the origin and quality of their food, while farmers gain fairer compensation for their labor and resources. 
+                    This movement underscores the importance of knowing where food comes from, how it's produced, and the impact of 
+                    its cultivation on the environment and local communities. It encourages consumers to forge connections with farmers 
+                    through farmers' markets, community-supported agriculture (CSA) programs, and direct purchasing arrangements. 
+                    Moreover, farm-to-table practices often prioritize seasonal and organic produce, as well as humane and environmentally 
+                    responsible animal husbandry practices. By fostering a deeper appreciation for the journey of food from farm to table, 
+                    this movement seeks to create a more sustainable and equitable food system for present and future generations.</p>
+            </div>
         </div>
     );
 }
