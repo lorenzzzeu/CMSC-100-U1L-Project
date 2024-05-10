8b6f1901df121c79311383152d858227441e3f83
pvgonzales <pvgonzales@up.edu.ph>
2024-05-10 17:17:39 +0800

    fix: fixed the navigation of customer page when signed out

--- src/pages/Customer/CustomerPage.jsx
+++ src/pages/Customer/CustomerPage.jsx
@@ -1,10 +1,11 @@
 // pages/CustomerHome.jsx
 import React from 'react';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
 import ProductList from '../../components/Customer/ProductList';
 import ShoppingCart from '../../components/Customer/ShoppingCart';
 import OrderList from '../../components/Customer/OrderList';
 import Root from '../Root';
+import Home from '../Home';
 
 const CustomerPage = () => {
   // Assume cartItems and orders are fetched from the server or passed as props
@@ -26,22 +27,22 @@ const CustomerPage = () => {
     // </div>
 
     <div>
-    <nav>
+    
       { isUserSignedIn ? (
-          <>
+          <nav>
             <Link to="/customer-page">Home</Link>
             <Link to="/customer-page/product-list">Product List</Link>
             <Link to="/customer-page/shopping-cart">Shopping Cart</Link>
             <Link to="/customer-page/order-list">Order List</Link>
             {/* must style button to match the other nav components */}
             <button onClick={handleLogout}>Log Out</button> 
-          </>
+          </nav>
         ) : (
           <>
-            <Root/>
+            <Navigate to ='/' />
           </>
         ) }
-    </nav>
+    
     <Outlet />
   </div>
   );
