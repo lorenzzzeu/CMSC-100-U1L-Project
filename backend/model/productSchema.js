5a31d1afa10fa0fd0337be0f1133d66a86f1afa0
pvgonzales <pvgonzales@up.edu.ph>
2024-05-12 17:12:11 +0800

    refactor: modified product schema

--- backend/model/productSchema.js
+++ backend/model/productSchema.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-    prodID: { type: Number, required: true },
     prodName: { type: String, required: true },
     prodType: { type: String, required: true },
     prodPrice: { type: Number, required: true },
     prodDesc: { type: String, required: true}, 
-    prodQuant: { type: Number, required: true }
+    prodQuant: { type: Number, required: true },
+    prodImage: { type: String, required: true }
 })
 
-const Product = mongoose.model('Product', userSchema)
+const Product = mongoose.model('Product', productSchema)
 
 module.exports = Product
