0002dd7dbc15bad7e474a4a79271c3d1f84a5a9b
pvgonzales <pvgonzales@up.edu.ph>
2024-05-17 20:58:26 +0800

    chore: modified fetchCartItems

--- src/pages/Customer/ShoppingCart.jsx
+++ src/pages/Customer/ShoppingCart.jsx
@@ -1,23 +1,28 @@
 
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const ShoppingCart = () => {
   const [cartItems, setCartItems] = useState([]);
   const [total, setTotal] = useState(0);
-  useEffect(() => {
-    fetchCartItems();
-  }, []);
-
-  const fetchCartItems = async () => {
-    try {
-      const response = await fetch('http://localhost:3001/cart-items');
-      const data = await response.json();
-      setCartItems(data);
-      calculateTotal(data);
-    } catch (error) {
-      console.error('Error fetching cart items:', error);
-    }
-  };
+    useEffect(() => {
+      fetchCartItems();
+    }, []);
+  
+    const fetchCartItems = async () => {
+      try {
+        const token = localStorage.getItem('token'); // Retrieve the token from local storage
+        const response = await axios.get('http://localhost:3001/cart-items', {
+          headers: {
+            'authorization': `Bearer ${token}`
+          }
+        });
+        setCartItems(response.data); // Set cart items from response
+        calculateTotal(response.data)
+      } catch (error) {
+        console.error('Error fetching cart items:', error.response ? error.response.data : error.message);
+      }
+    };
   
   const calculateTotal = (items) => {
     const totalPrice = items.reduce((acc, item) => acc + (item.prodPrice * item.prodQuant), 0);
