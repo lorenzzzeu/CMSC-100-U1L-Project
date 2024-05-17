9606b2b5bf1e38b4b0449ace4d36b414a2c83dff
pvgonzales <pvgonzales@up.edu.ph>
2024-05-18 02:34:30 +0800

    chore: added the functionalities for order details

--- src/pages/Customer/OrderList.jsx
+++ src/pages/Customer/OrderList.jsx
@@ -1,8 +1,42 @@
 
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const OrderList = () => {
-  const order = [];
+  const [orderList, setOrderList] = useState([]);
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.get('http://localhost:3001/order-transaction', {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
+      });
+      setOrderList(response.data);
+    } catch (error) {
+      console.error('Error fetching orders:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  const handleCancelOrder = async (orderId) => {
+    try {
+      const token = localStorage.getItem('token');
+      await axios.delete(`http://localhost:3001/order-transaction/${orderId}`, {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
+      });
+      // Remove the cancelled order from the orderList state
+      setOrderList(orderList.filter(order => order.ordTransId !== orderId));
+    } catch (error) {
+      console.error('Error cancelling order:', error.response ? error.response.data : error.message);
+    }
+  };
 
   return (
     <>
@@ -11,6 +45,23 @@ const OrderList = () => {
       <h1>ORDER LIST</h1>
     </div>
     <hr/>
+    <div>
+    <ul>
+        {orderList.map((order) => (
+          <li key={order.ordTransId}>
+            <div>{order.ordTransId}</div>
+            <div>{order.ordDate}</div>
+            <div>{order.time}</div>
+            <div>{order.ordStatus}</div>
+            {order.ordStatus === 'Pending' ? (
+              <button onClick={() => handleCancelOrder(order.ordTransId)}>Cancel</button>
+            ): (
+              <div></div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
     </>
   );
 };
