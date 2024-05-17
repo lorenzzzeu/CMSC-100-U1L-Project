fc98b9538afd34d183442f10a1e98df15c7f1d2a
pvgonzales <pvgonzales@up.edu.ph>
2024-05-17 17:48:54 +0800

    chore: modified shopping cart component to display the items added

--- src/pages/Customer/ShoppingCart.jsx
+++ src/pages/Customer/ShoppingCart.jsx
@@ -1,9 +1,28 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ShoppingCart = () => {
+  const [cartItems, setCartItems] = useState([]);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
 
-  const cart = [];
+  const fetchCartItems = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/cart-items');
+      const data = await response.json();
+      setCartItems(data);
+      calculateTotal(data);
+    } catch (error) {
+      console.error('Error fetching cart items:', error);
+    }
+  };
+  
+  const calculateTotal = (items) => {
+    const totalPrice = items.reduce((acc, item) => acc + (item.prodPrice * item.prodQuant), 0);
+    setTotal(totalPrice);
+  };
 
   return (
     <>
@@ -12,6 +31,26 @@ const ShoppingCart = () => {
       <h1>SHOPPING CART</h1>
     </div>
     <hr/>
+    <div>
+      <h2>Cart Items</h2>
+      {cartItems.length === 0 ? (
+        <p>No items in the cart.</p>
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.prodId}>
+              <img src={item.prodImage} alt={item.prodName} width="50" />
+              <h3>{item.prodName}</h3>
+              <p>Price: ${item.prodPrice}</p>
+              <button>REMOVE</button>
+            </li>
+          ))}
+          <div>
+            <h2>Total: {total}</h2>
+          </div>
+        </ul>
+      )}
+    </div>
     </>
 
   );
