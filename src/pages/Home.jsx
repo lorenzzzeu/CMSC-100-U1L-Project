f9c11417c9aa6d1d84d7b968c9ae6da6ba3d5c48
naanareta <naanareta@up.edu.ph>
2024-05-20 12:02:02 +0800

    ui:homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,44 @@
 // src/pages/Home.jsx
 import React from 'react';
 import Login from './Login';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Explore from './Explore';
 
 function Home() {
 
   const navigate = useNavigate();
 
-  const goToNewPage = () => {
-    navigate('/explore')
+  const goToLogin = () => {
+    navigate('/login')
   }
 
-
   return (
+    <>
     <div className='appContainer'>
-      <div className='main-content'>
-        <h1>WELCOME TO</h1>
-        <h1 className='welcome2'>Farm-to-Table E-commerce Website</h1>
-        <button onClick={goToNewPage}>EXPLORE NOW</button>
-      </div>
-      <Login/>
+      <nav className='navRoot'>
+        <Link to="/admin-page"><img className='logoHeader' src='/src/img/100-logo.png'/></Link>
+        <a className='nav-item' href='#main-content'>Home</a>
+        <a className='nav-item' href='#about'>About</a>
+        <a className='nav-item' href='#contact'>Contact</a>
+      </nav>
+      <section id='main-content'>
+        <div className='main-content-text'>
+          <h1>FARM-TO-TABLE</h1>
+          <h2>E-Commerce Website</h2>
+          <p>The "farm-to-table" movement advocates for a direct connection between consumers and 
+            agricultural producers, promoting sustainability, transparency, and community engagement</p>
+          <button onClick={goToLogin}>Get Started</button>
+        </div>
+        <img className='homeImg' src='/src/img/meal.png'/>
+      </section>
+      <section id='about'>
+        <Explore/>
+      </section>
+      <section id='contact'>
+
+      </section>
     </div>
+    </>
   );
 };
 
