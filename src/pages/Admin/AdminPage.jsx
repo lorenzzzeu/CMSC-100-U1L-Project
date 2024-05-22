080ba76f31bb0d42b2e569622b94aaa2422ef697
pvgonzales <pvgonzales@up.edu.ph>
2024-05-22 21:25:02 +0800

    fix: fixed the accessibility of admin page

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -4,7 +4,8 @@ import { Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
   const navigate = useNavigate()
-
+  const isUserSignedIn = !!localStorage.getItem('token')
+  
   const handleLogout = () => {
     localStorage.removeItem('token')
     navigate('/')
@@ -12,18 +13,26 @@ const AdminPage = () => {
 
   return (
     <div>
-      <nav className='navRoot'>
-        <Link to="/admin-page"><img className='logoHeader' src="src/img/100-logo.png"/></Link>
-        <Link to="/admin-page/user-management" className='nav-item'>User Management</Link>
-        <Link to="/admin-page/product-listings" className='nav-item'>Product Listings</Link>
-        <Link to="/admin-page/order-fulfillment" className='nav-item'>Order Fulfillment</Link>
-        <Link to="/admin-page/sales-reports" className='nav-item'>Sales Report</Link>
-        <div className='navUser'>
-          <button onClick={handleLogout}>LOG OUT</button>
-        </div>
-      </nav>
-      <Outlet />
-    </div>
+    {isUserSignedIn ? (
+      <div>
+        <nav className='navRoot'>
+          <Link to="/admin-page"><img className='logoHeader' src="src/img/100-logo.png"/></Link>
+          <Link to="/admin-page/user-management" className='nav-item'>User Management</Link>
+          <Link to="/admin-page/product-listings" className='nav-item'>Product Listings</Link>
+          <Link to="/admin-page/order-fulfillment" className='nav-item'>Order Fulfillment</Link>
+          <Link to="/admin-page/sales-reports" className='nav-item'>Sales Report</Link>
+          <div className='navUser'>
+            <button onClick={handleLogout}>LOG OUT</button>
+          </div>
+        </nav>
+        <Outlet/>
+      </div>
+      ) : (
+        <>
+        <Navigate to = '/'/>
+        </>
+      )}
+  </div>
   );
 };
 
