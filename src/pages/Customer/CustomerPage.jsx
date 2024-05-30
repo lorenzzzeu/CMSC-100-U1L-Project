8da5284a4424a3c6ab9a008f227c241c55693561
lorenzzzeu <lggabriel1@up.edu.ph>
2024-05-31 01:09:57 +0800

    docs: added documentation for CustomerPage.jsx; refactor: removed unnecessary codes

--- src/pages/Customer/CustomerPage.jsx
+++ src/pages/Customer/CustomerPage.jsx
@@ -1,50 +1,53 @@
 // pages/CustomerHome.jsx
+
+// Importing necessary dependencies from React and React Router
 import React from 'react';
 import { Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
-
+// Defining the functional component CustomerPage
 const CustomerPage = () => {
-  // Assume cartItems and orders are fetched from the server or passed as props
-  const cartItems = [];
-  const orders = [];
+  
+  const isUserSignedIn = !!localStorage.getItem('token');   // Checking if the user is signed in by looking for a token in localStorage
+  const navigate = useNavigate();   // Using the useNavigate hook to programmatically navigate
 
-  const isUserSignedIn = !!localStorage.getItem('token')
-  const navigate = useNavigate()
-
+  // Defining the handleLogout function to remove the token and navigate to the home page
   const handleLogout = () => {
-    localStorage.removeItem('token')
-    navigate('/')
+    localStorage.removeItem('token'); // Remove token from localStorage
+    navigate('/'); // Navigate to the home page
   };
   
+  // Returning the JSX for the component
   return (
-  <div>
-    {isUserSignedIn ? (
+    <div>
+      {isUserSignedIn ? ( // Conditional rendering based on whether the user is signed in
         <div>
           <nav className='navRoot'>
             <div className='navLinks'>
-              <Link to="/customer-page">
-                <img className='logoHeader' src="/src/img/100-logo.png"/>
+              <Link to="/customer-page"> {/* Link to customer page */}
+                <img className='logoHeader' src="/src/img/100-logo.png"/> {/* Company logo */}
               </Link>
-              <Link to="/customer-page/product-list" className='nav-item'>Product List</Link>
-              <Link to="/customer-page/shopping-cart" className='nav-item'>Shopping Cart</Link>
-              <Link to="/customer-page/order-list" className='nav-item'>Order List</Link>
+              <Link to="/customer-page/product-list" className='nav-item'>Product List</Link> {/* Link to product list */}
+              <Link to="/customer-page/shopping-cart" className='nav-item'>Shopping Cart</Link> {/* Link to shopping cart */}
+              <Link to="/customer-page/order-list" className='nav-item'>Order List</Link> {/* Link to order list */}
             </div>
             <div className='navUser'>
-              <Link to="/customer-page/profile" className='profile'><FontAwesomeIcon icon={faCircleUser} color='black' size='2x'/></Link>
-              <button onClick={handleLogout}>LOG OUT</button>
+              <Link to="/customer-page/profile" className='profile'> {/* Link to profile */}
+                <FontAwesomeIcon icon={faCircleUser} color='black' size='2x'/> {/* User icon */}
+              </Link>
+              <button onClick={handleLogout}>LOG OUT</button> {/* Logout button */}
             </div>
           </nav>
-          <Outlet />
+          <Outlet /> {/* Render nested routes here */}
         </div>
-      ) : (
+      ) : ( // If user is not signed in
         <>
-        <Navigate to = '/'/>
+          <Navigate to='/'/> {/* Redirect to home page */}
         </>
       )}
-  </div>
+    </div>
   );
 };
 
-export default CustomerPage;
+export default CustomerPage; // Exporting the CustomerPage component as the default export
